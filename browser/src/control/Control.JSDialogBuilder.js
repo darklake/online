863949e301ac8e9a2bec23f620b0db21c1b788ff
krchoi <krchoi@cyber-line.co.kr>
2023-10-17 16:08:55 +0900

    trans

--- browser/src/control/Control.JSDialogBuilder.js
+++ browser/src/control/Control.JSDialogBuilder.js
@@ -534,7 +534,8 @@ L.Control.JSDialogBuilder = L.Control.extend({
 		if (data.step != undefined) {
 			// we don't want to show error popups due to browser step validation
 			// so be sure all the values will be acceptted, check only precision
-			/*	// KRCHOI 모든 undefined step 에는 1 적용. 0.0001이 뭐야 대체.. 
+
+			/*	KRCHOI 모든 undefined step 에는 1 적용. 0.0001이 뭐야 대체.. 
 			var step = getPrecision(data.step);
 			var value = data.value ? getPrecision(data.value) : 1;
 			var minStep = getPrecision(data.min);
@@ -542,9 +543,8 @@ L.Control.JSDialogBuilder = L.Control.extend({
 
 			step = Math.min(step, value, minStep, maxStep);
 			*/
-			step = 1;
 
-			$(spinfield).attr('step', step);
+			$(spinfield).attr('step', 1);
 		}
 
 		if (data.enabled === 'false' || data.enabled === false) {

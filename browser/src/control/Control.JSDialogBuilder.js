9abfd99f2b58b00ff42caf6a33964d8959b5e237
krchoi <krchoi@cyber-line.co.kr>
2023-10-17 15:57:18 +0900

    trans

--- browser/src/control/Control.JSDialogBuilder.js
+++ browser/src/control/Control.JSDialogBuilder.js
@@ -534,7 +534,7 @@ L.Control.JSDialogBuilder = L.Control.extend({
 		if (data.step != undefined) {
 			// we don't want to show error popups due to browser step validation
 			// so be sure all the values will be acceptted, check only precision
-			var step = getPrecision(data.step);
+			var step = 1; //getPrecision(data.step);
 			var value = data.value ? getPrecision(data.value) : 1;
 			var minStep = getPrecision(data.min);
 			var maxStep = getPrecision(data.max);
